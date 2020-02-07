83dde5552ab279f40b086ccd6473382d9dd6ba2b
amal-san <amaldemaniax@gmail.com>
2020-02-07 14:31:36 +0530

    test page change for url

---
+++ src/pages/typerex/test.js
@@ -0,0 +1,23 @@
+import React from "react"
+import '../../css/index.css'
+import Header from '../../components/Header'
+import { Helmet } from "react-helmet"
+
+
+const SecondPage = () => (
+	<>
+    <title>Typerex </title>
+    <Header />
+     <div className='test-container'> 
+     	<div className='test-card'>
+     		<div className='para-card'></div>
+     		<div className='test-enter'></div>
+     	</div>
+     	<div className='test-below'></div>
+     </div>
+    
+    </>
+  
+)
+
+export default SecondPage
