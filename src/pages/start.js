debf533c0342fe56af87e4e8a7d751f3a1bf3b00
amal-san <amaldemaniax@gmail.com>
2020-02-12 21:04:36 +0530

    api fetch start component

--- src/pages/start.js
+++ src/pages/start.js
@@ -4,20 +4,100 @@ import Header from '../components/Header'
 import { Helmet } from "react-helmet"
 
 
-const SecondPage = () => (
-	<>
-    <title>Typerex </title>
-    <Header />
-     <div className='test-container'> 
-     	<div className='test-card'>
-     		<div className='para-card'></div>
-     		<div className='test-enter'></div>
-     	</div>
-     	<div className='test-below'></div>
-     </div>
+
+
+
+
+class SecondPage extends React.Component {
+
+    constructor(){
+        super()
+        this.state={
+            text:[],
+            words:[],
+            isDataSet:true,
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+   }
+   
+
+   fetchPara()
+   {
+        // URL
+        const url = "https://typeracingapi.rishikc.com/";
+
+        // Request
+        fetch(url).then(res => {
+            if(res.status == 200) {
+                return res.json()
+            }
+            else return null;
+        }).then(data => {
+            console.log(data)
+            if(data!==null)  {
+                this.setState({
+                    text: data.text,
+                    words: data.text.split(' '),
+                    isDataSet: true
+                });
+            } else {
+                this.setState({
+                    isDataSet: false
+                });
+            }
+        });
+
+
+
+    }
+
+   handleClick() {
+
+        let value = document.getElementById('paraEnter').value;
+        console.log(this.state.words)
+        // for(var j=0,j<this.state.text.length,j++){
+           
+        // }
+
+   }
+   componentDidMount() {
+    this.fetchPara()
+
+   }
+
+    render(){
+
+        if(this.state.isDataSet){
+            var rows = [];
+            for (var i = 0; i < this.state.text.length; i++) {
+                rows.push(<span id={i} key={i}>{this.state.text[i]}</span>);
+            }
+        }
+
+        return (
+
+                <>
+                <title>Typerex </title>
+                <Header />
+                 <div className='test-container'> 
+                    <div className='test-card'>
+                        <div className='para-card'>
+                            {this.state.isDataSet ? rows : 'Not working... Please refresh'}
+                        </div>
+                    <div className='test-enter'>
+                    <input id='paraEnter' onChange={this.handleClick} type='text' placeholder='Start typing......'></input>
+                    </div>
+                    </div>
+                    <div className='test-below'></div>
+                 </div>                
+                 </>
+            )
+    }  
+
+
     
-    </>
   
-)
+}
 
 export default SecondPage
