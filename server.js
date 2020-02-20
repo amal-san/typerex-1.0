8b4f87ed994828bb4f0be4a76fb97aaf358e6e6e
amal-san <amaldemaniax@gmail.com>
2020-02-20 22:08:22 +0530

    graphql data rendering

--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
-
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String,
-    hai: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-  hai: () => {
-    return "Hai woasdf";
-  },
-};
-
-var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
 
 
 const dbName = 'typerex';
@@ -53,11 +23,12 @@ async function main(){
  
         // Make the appropriate DB calls
         // await  listDatabases(client);
-        if (await CheckUser(client,username)){
+        if (await CheckUsername(client,username)){
 
           await InsertUser(client,username,password)
         }
         else {
+          await listDatabases(client)
           console.log('username already taken!!')
         }
 
@@ -74,16 +45,16 @@ main().catch(console.error);
 
 async function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
- 
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+    console.log(databasesList.databases[0].name)
+    return databasesList
 };
 
 
-async function CheckUser(client,username) {
-
-    result = await client.db(dbName).collection(collectionName).findOne({ usename: username }
-      );
+async function CheckUsername(client,username) {
+  
+  result = await client.db(dbName).collection(collectionName).findOne({ usename: username });
  
     if (result) {
         console.log(result);
@@ -95,7 +66,54 @@ async function CheckUser(client,username) {
 }
 
 async function InsertUser(client,username,password) {
+
   await client.db(dbName).collection(collectionName).insertOne({ username: username ,wpm : null ,password: password})
+
   console.log('Account created with username %s', username)
 
 }
+
+
+async function CheckUser(client,username) {
+  
+  result = await client.db(dbName).collection(collectionName).findOne({ usename: username ,password: password});
+ 
+    if (result) {
+        console.log(result);
+        return result
+
+    } else {
+      return false
+    }
+}
+
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    list: String ,
+    hai: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  list: () => {
+    return databasesList.databases[0].name
+  },
+  hai: () => {
+    return "Hai woasdf";
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
