ff3b5a24a5aef99f923e108a8bb0cfe93d2c0bb7
amal-san <amaldemaniax@gmail.com>
2020-02-21 22:47:03 +0530

    loading bar customization

--- src/components/Myloader.js
+++ src/components/Myloader.js
@@ -4,16 +4,16 @@ import ContentLoader from "react-content-loader"
 const MyLoader = () => (
  <ContentLoader 
     speed={2}
-    width={500}
+    width={1500}
     height={100}
-    viewBox="0 0 500 100"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
   >
-    <rect x="3" y="100" rx="3" ry="3" width="100%" height="7" /> 
-    <rect x="0" y="70" rx="3" ry="3" width="475" height="7" /> 
-    <rect x="-2" y="85" rx="3" ry="3" width="479" height="7" /> 
-
+    <rect x="10" y="10" rx="5" ry="2" width="1300" height="8" />
+    <rect x="10" y="30" rx="5" ry="2" width="1000" height="8" />
+    <rect x="10" y="50" rx="5" ry="2" width="900" height="8" />
+    <rect x="10" y="70" rx="5" ry="2" width="700" height="8" />
+    <rect x="10" y="90" rx="5" ry="2" width="500" height="8" />
   </ContentLoader>
 )
 
