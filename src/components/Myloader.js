e6248e646fea90d174b660b58bbb6eddb3b88b37
amal-san <amaldemaniax@gmail.com>
2020-03-08 20:27:31 +0530

    same width for loader

--- src/components/Myloader.js
+++ src/components/Myloader.js
@@ -10,10 +10,10 @@ const MyLoader = () => (
     foregroundColor="#ecebeb"
   >
     <rect x="10" y="10" rx="5" ry="2" width="1300" height="8" />
-    <rect x="10" y="30" rx="5" ry="2" width="1000" height="8" />
-    <rect x="10" y="50" rx="5" ry="2" width="900" height="8" />
-    <rect x="10" y="70" rx="5" ry="2" width="700" height="8" />
-    <rect x="10" y="90" rx="5" ry="2" width="500" height="8" />
+    <rect x="10" y="30" rx="5" ry="2" width="1300" height="8" />
+    <rect x="10" y="50" rx="5" ry="2" width="1300" height="8" />
+    <rect x="10" y="70" rx="5" ry="2" width="1300" height="8" />
+    <rect x="10" y="90" rx="5" ry="2" width="1300" height="8" />
   </ContentLoader>
 )
 
