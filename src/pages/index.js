4e39beb4d4c0c39ed1cdf7682c00e465098c39b5
amal-san <amaldemaniax@gmail.com>
2020-03-28 11:20:31 +0530

    main

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,18 @@ import { Link } from "gatsby"
 import '../css/index.css'
 import Header from '../components/Header'
 import Home from '../components/Home'
+import PageTransition from 'gatsby-plugin-page-transitions';
+
 
 const IndexPage = () => (
+   
+   <PageTransition>
     <>
     <Header />
     <Home />
     
     </>
+   </PageTransition>
   
 )
 
