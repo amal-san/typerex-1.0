886421dba44d6c9b2c82143de5ebd20bf41aaeda
amal-san <amaldemaniax@gmail.com>
2020-03-28 10:28:03 +0530

    animation add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from "react"
 import '../css/index.css'
+import PageTransition from 'gatsby-plugin-page-transitions';
+
 
 const Header = () => (
   
-    
+  	<PageTransition>
     	<nav>
     		<div className ='nav'>
 	    	 	<div className='nav-item'><a href='/typerex' style={{textDecoration:'none',color:'white'}}> typerex </a> </div>
     	 	</div>
     	</nav> 
-    
+      </PageTransition>
   
 )
 
