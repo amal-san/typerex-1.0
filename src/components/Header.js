9358291f7a24bb4f03fb804f315a6fc80de79bf0
amal-san <amaldemaniax@gmail.com>
2020-04-25 20:43:02 +0530

    right side menu for user

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,35 @@ import '../css/index.css'
 import PageTransition from 'gatsby-plugin-page-transitions';
 
 
-const Header = () => (
-<nav>
-	<div className="nav">
-		<div className="nav-item">
-			<a
-				href="/typerex"
-				style={{ textDecoration: "none", color: "white" }}
-			>
-				{" "}
-				typerex{" "}
-			</a>{" "}
-		</div>
-	</div>
-</nav>
-)
+class Header extends React.Component {
+
+	constructor(){
+		super()
+	
+	}
+
+
+	render() {
+		return(
+		<nav>
+			<div className="nav">
+				<div className='user-item'></div>
+				<div className="nav-item">
+					<a
+						href="/typerex"
+						style={{ textDecoration: "none", color: "white" }}
+					>
+
+						typerex
+					</a>
+				</div>
+				<div className='user-item-main'>
+					<p style={{marginBottom:'0'}}><b id='username'></b></p>
+					<p id='user-wpm' style={{margin:'0'}}></p>
+				 </div>
+			</div>
+		</nav>)
+	}
+}
 
 export default Header
