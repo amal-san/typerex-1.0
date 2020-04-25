73384d6fe7cc0612912acb66795cfdb843ce0d17
amal-san <amaldemaniax@gmail.com>
2020-04-25 20:44:55 +0530

    class component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,18 +4,51 @@ import '../css/index.css'
 import Header from '../components/Header'
 import Home from '../components/Home'
 import PageTransition from 'gatsby-plugin-page-transitions';
+import axios from 'axios'
+import cogoToast from 'cogo-toast';
 
 
-const IndexPage = () => (
-   
-   <PageTransition>
-    <>
-    <Header />
-    <Home />
-    
-    </>
-   </PageTransition>
-  
-)
+class IndexPage extends React.Component {
+	constructor(){
+		super()
+	}
+	componentDidMount() {
+
+		axios.post("http://localhost:8080/infoUser/", { username:window.localStorage.getItem('typerex_username') })
+        .then( (res) => {
+          console.log(res.data)
+          if(res.data !== 'no_user' ) {
+          	
+          	window.location.href = "http://localhost:8000/start/";
+      
+         }
+         else {
+
+          console.warn('no_user')
+
+          }
+        })
+        .catch((e) =>{
+          console.log(e)
+         });
+
+	}
+	render(){
+		return(
+
+			   <PageTransition>
+			    <>
+			    <Header />
+			    <Home />
+			    
+			    </>
+			   </PageTransition>
+
+
+
+			)
+	}
+}
+
 
 export default IndexPage
