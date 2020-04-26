0352da3b03463bbd26d75375f6cafee1f696558e
amal-san <amaldemaniax@gmail.com>
2020-04-26 11:24:56 +0530

    url,wpm and username handling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
 import '../css/index.css'
 import Header from '../components/Header'
 import Home from '../components/Home'
 import PageTransition from 'gatsby-plugin-page-transitions';
 import axios from 'axios'
-import cogoToast from 'cogo-toast';
+import history from '../components/History';
+
 
 
 class IndexPage extends React.Component {
 	constructor(){
 		super()
+		this.state ={
+			shouldload:false
+		}
 	}
 	componentDidMount() {
 
 		axios.post("http://localhost:8080/infoUser/", { username:window.localStorage.getItem('typerex_username') })
         .then( (res) => {
-          console.log(res.data)
+          this.setState({shouldload:false})
           if(res.data !== 'no_user' ) {
-          	
-          	window.location.href = "http://localhost:8000/start/";
+          	history.push('/typerex/start/');
+
       
          }
          else {
 
           console.warn('no_user')
+          this.setState({shouldload:true})
+
 
           }
         })
         .catch((e) =>{
-          console.log(e)
+          this.setState({shouldload:true})
          });
 
 	}
 	render(){
-		return(
 
-			   <PageTransition>
+	let index =  
+		       <PageTransition>
 			    <>
 			    <Header />
 			    <Home />
@@ -46,6 +51,12 @@ class IndexPage extends React.Component {
 
 
 
+		return(
+			<>
+
+			  {this.state.shouldload ? index : ''}
+
+			</>
 			)
 	}
 }
