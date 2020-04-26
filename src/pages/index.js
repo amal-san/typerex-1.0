a8c89b873062bd5ed1d76795bbb16196d6587381
amal-san <amaldemaniax@gmail.com>
2020-04-26 13:34:17 +0530

    url change

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ class IndexPage extends React.Component {
 	}
 	componentDidMount() {
 
-		axios.post("https/typerex.herokuapp.com/infoUser/", { username:window.localStorage.getItem('typerex_username') })
+		axios.post("https://typerex.herokuapp.com/infoUser/", { username:window.localStorage.getItem('typerex_username') })
         .then( (res) => {
           this.setState({shouldload:false})
           if(res.data !== 'no_user' ) {
