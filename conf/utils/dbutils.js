56a993f20218f41175bbe23efcd7d5a2c604ac6b
amal-san <amaldemaniax@gmail.com>
2020-05-05 15:55:12 +0530

    made compatible with graphql functions

--- conf/utils/dbutils.js
+++ conf/utils/dbutils.js
@@ -12,6 +12,25 @@ module.exports = {
 		return databasesList.databases;
 	},
 
+
+	listUsers: async function(dbName,collectionName,client){
+		result = await client
+				.db(dbName)
+				.collection(collectionName)
+				.find({});
+
+		if(result){
+			console.log("Returning results")
+			return result
+		}
+		else {
+			return {info:"can't find the list"}
+		}
+
+
+
+	},
+
 	// Username check Method
 	addUser: async function(dbName, collectionName, client, username) {
 		//checks whether username is not added and wpm null
@@ -33,11 +52,12 @@ module.exports = {
 			// A user with not null wpm can't create account
 			if (newuser) {
 				console.log('login')
-				return "login"
+				return result
+				// return "login"
 
 			} else {
-
-				return "username_taken"
+				return result
+				// return "username_taken"
 			}
 		} else {
 			console.log("Add user function false: ", result);
@@ -51,9 +71,12 @@ module.exports = {
 			.db(dbName)
 			.collection(collectionName)
 			.insertOne({ username: username, wpm: null });
+
 		if (result) {
+
 			console.log("Account created with username %s", username);
-			return "user_created";
+			return this.infoUser(dbName,collectionName,client,username);
+			// return 'user_created'
 		} else {
 			console.log(result);
 			return "username_taken";
@@ -74,21 +97,53 @@ module.exports = {
 	},
 
 	updateUser: async function(dbName, collectionName, client, username, wpm) {
-		result = await client
+		check = await client
 			.db(dbName)
 			.collection(collectionName)
-			.updateOne(
-				{ username: username },
-				{ $set: { wpm:wpm } },
-				{ upsert: true }
-			);
-		if (result) {
-			console.log(result)
+			.findOne({ username: username });
+
+
+		if (check) {
+		    result = await client
+				.db(dbName)
+				.collection(collectionName)
+				.updateOne(
+					{ username: username },
+					{ $set: { wpm:wpm } },
+					{ upsert: true }
+				);
 			console.log("Updated Wpm with username %s", username);
-			return "user_updated";
+			return this.infoUser(dbName,collectionName,client,username);
+
+			// return "user_updated";
+
 		} else {
-			console.log(result);
-			return "user_not_update";
+			return { info:'user_not_found' }
+			// console.log(result);
+			// return "user_not_update";
+		}
+	},
+
+
+	deleteUser: async function(dbName,collectionName,client,username) {
+		check = await client
+			.db(dbName)
+			.collection(collectionName)
+			.findOne({ username: username });
+
+		if(check) {
+			result = await client
+			.db(dbName)
+			.collection(collectionName)
+			.findOneAndDelete({username:username})
+			console.log("Deleted user with username %s ",username)
+			return {username:username}
+		}
+		else {
+
+			return {info:'user not found'}
+			// console.log("Can't Delete user")
+			// return "user_not_deleted"
 		}
 	},
 
