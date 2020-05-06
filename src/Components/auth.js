dab5039e3cba9b8f45ecb8a59814b15735178c1b
amal-san <amaldemaniax@gmail.com>
2020-05-06 20:15:38 +0530

    auth handling

--- src/Components/auth.js
+++ src/Components/auth.js
@@ -1,8 +1,80 @@
 import { Query } from '@apollo/react-components';
 import { gql } from 'apollo-boost';
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 
 
 
+
+const client = new ApolloClient({
+  uri: 'https://typerex.herokuapp.com/',
+});
+
+
+
+class Auth {
+
+	constructor(){
+		this.authenticated = false;
+	}
+
+	async login(username) {
+		const result = await client
+		  .mutate({
+		  	mutation:gql`
+		  	mutation {
+				  userAdd(username:"${username}"){
+				    username
+				    wpm
+					}
+				}
+		  	`
+		  })
+		  .then(function(result){
+		  	return result;
+		  })
+		  .catch(e => console.log(e))
+		  if (result) {
+			this.authenticated = true;
+		  }
+   		  return result;
+	}
+
+	async userInfo(username) {
+		const result = await client
+		  .query({
+		    query: gql`
+		     {
+			  userInfo(username:"${username}"){
+			    username
+			    wpm
+			  }
+			 }
+		    `
+		  })
+		  .then(function(result) {
+		  	return result
+		  });
+
+		if (result) {
+			this.authenticated = true;
+		}
+		return result;
+	}
+
+
+	logout(cb) {
+		this.authenticated = false;
+		cb()
+	}
+
+	isAuthenticated() {
+		return this.authenticated;
+	}
+
+
+}
 
 
 const USERINFO = gql`
@@ -14,13 +86,16 @@ const USERINFO = gql`
 }`
 
 
-const USERUPDATE;
+// const USERUPDATE;
 
 
-const USERDELETE; 
+// const USERDELETE; 
 
 
-const USERADD;
+// const USERADD;
 
 
-const USERLIST;
+// const USERLIST;
+
+
+export default new Auth();
